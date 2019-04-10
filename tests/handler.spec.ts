9f0f7483f0772644af9a17aab28c53cd7385bf97
Cole Schneider <colekschneider@gmail.com>
2019-04-10 05:55:30 -0700

    try this

--- tests/handler.spec.ts
+++ tests/handler.spec.ts
@@ -1,33 +1,30 @@
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import { expect } from 'chai';
+import { run } from '../src/handler';
+import { successMessage } from '../utils/asyncMiddleware';
 
-import { APIGatewayEvent, Context } from "aws-lambda";
-import { expect } from "chai";
-import { run } from "../src/handler";
-import { successMessage } from "../utils/asyncMiddleware";
-
-
-describe("Subscribe Handler", () => {
+describe('Subscribe Handler', () => {
   describe('[POST] Handler', () => {
-    it("should handle error if body isn't present", async() => {
-      const event = {body: null} as APIGatewayEvent;
-      const context = {} as Context
+    it("should handle error if body isn't present", async () => {
+      const event = { body: null } as APIGatewayEvent;
+      const context = {} as Context;
       const result = await run(event, context);
-      expect(result.statusCode).to.equal(400)
-      expect(JSON.parse(result.body)).to.deep.equal({ message: 'Email is required' })
+      expect(result.statusCode).to.equal(400);
+      expect(JSON.parse(result.body)).to.deep.equal({ message: 'Email is required' });
     });
-    it("should handle error if email isn't present", async() => {
-      const event = {body: JSON.stringify({})} as APIGatewayEvent;
-      const context = {} as Context
+    it("should handle error if email isn't present", async () => {
+      const event = { body: JSON.stringify({}) } as APIGatewayEvent;
+      const context = {} as Context;
       const result = await run(event, context);
-      expect(result.statusCode).to.equal(400)
-      expect(JSON.parse(result.body)).to.deep.equal({ message: 'Email is required' })
+      expect(result.statusCode).to.equal(400);
+      expect(JSON.parse(result.body)).to.deep.equal({ message: 'Email is required' });
     });
-    it("should handle the success if an email is present", async() => {
-      const event = {body: JSON.stringify({email_address: 'testing@gmail.com'})} as APIGatewayEvent;
-      const context = {} as Context
+    it('should handle the success if an email is present', async () => {
+      const event = { body: JSON.stringify({ email_address: 'testing@gmail.com' }) } as APIGatewayEvent;
+      const context = {} as Context;
       const result = await run(event, context);
-      expect(result.statusCode).to.equal(200)
-      expect(JSON.parse(result.body)).to.deep.equal(successMessage)
-    });  
-  })
-  
+      expect(result.statusCode).to.equal(200);
+      expect(JSON.parse(result.body)).to.deep.equal(successMessage);
+    });
+  });
 });
